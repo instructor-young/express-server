ab17269ac39fe405c9412dff1bad3eb85027a4dc
Yoo Jin Young <yoojinyoung@kakao.com>
2024-02-12 12:31:45 +0900

    chore: add health-check endpoint

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -6,6 +6,8 @@ import usersController from "./users.controller";
 
 const controllers = Router();
 
+controllers.get("/health-check", (_, res) => res.status(200).send());
+
 controllers.use("/auth", authController);
 controllers.use("/cart", cartController);
 controllers.use("/products", productsController);
